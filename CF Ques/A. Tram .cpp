8432e155cd30d13d88a5ad0ac0f5d430ea613d62
namanr-dev <namanr.developer@gmail.com>
2026-01-16 21:58:53 +0530

    Jan 16th 2026

---
+++ CF Ques/A. Tram .cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int current = 0, capacity = 0;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        current -= a;   // passengers exit
+        current += b;   // passengers enter
+
+        capacity = max(capacity, current);
+    }
+
+    cout << capacity << endl;
+    return 0;
+}
