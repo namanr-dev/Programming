af3b9c329b438481adbaebdfd67c4090aa16080d
namanr-dev <namanr.developer@gmail.com>
2026-01-19 23:30:05 +0530

    Jan 19th 2026

---
+++ CF Ques/new.py
@@ -0,0 +1,15 @@
+f=open("diary.txt",'w')
+f.write("hello aaaaaaaaaaaaaaaa dfsdgsd")
+f.close()
+
+def any():
+    f=open("diary.txt",'r')
+    s=f.read()
+    w=s.split()
+    count=0   
+    for i in w:
+        if len(i)<4:
+            count=count+1
+    print("the no of words more than 4 character are",count)
+
+any()
