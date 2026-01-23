1f4e39afcf6557dad2516df361ee9dc18dfb5831
namanr-dev <namanr.developer@gmail.com>
2026-01-23 22:03:21 +0530

    Jan 23th 2026

---
+++ CF Ques/B. Little Girl and Game.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<int> freq(26, 0);
+    for (char c : s) freq[c - 'a']++;
+
+    int odd = 0;
+    for (int f : freq) {
+        if (f % 2 == 1) odd++;
+    }
+
+    if (odd <= 1 || odd % 2 == 1)
+        cout << "First\n";
+    else
+        cout << "Second\n";
+
+    return 0;
+}
